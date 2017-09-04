57bad8e039e52957cd055db6d65232e6f5ef63bf
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-04 12:51:02 -0500

    embed youtube videos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'; // used to create and manage components
 import ReactDOM from 'react-dom'; // used to inteareact witht he browser
-import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCbl40Ks3o3c5xRkh3iWs3zoqW-uoAHe64';
 
 class App extends Component {
@@ -11,12 +13,13 @@ class App extends Component {
         this.state = { videos: [] };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, videos => this.setState({ videos }));
-
     }
     render() {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/> 
             </div>
         );
     }
