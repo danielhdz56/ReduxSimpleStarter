8a37ff2ceebe80513088e7513e89a9258fe221cc
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-04 11:44:09 -0500

    made app class based component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react'; // used to create and manage components
+import React, { Component } from 'react'; // used to create and manage components
 import ReactDOM from 'react-dom'; // used to inteareact witht he browser
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyCbl40Ks3o3c5xRkh3iWs3zoqW-uoAHe64';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data));
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
 
-// Create a new component. This component should produce some HTML.
-// This is a functional component because it doesn't have any concept of state
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => this.setState({ videos }));
+
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
